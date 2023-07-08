ed8860adff58f1350d8ce0b0bd8e88a477fb1330
Ton Ancheta <ton@xurpas.com>
2023-07-08 21:11:17 +0800

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Weekly Planner
+
+Weekly Planner for Monstessori Guides
+
+#### License
+
+MIT
---
+++ weekly_planner/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Weekly Planner",
+			"type": "module",
+			"label": _("Weekly Planner")
+		}
+	]
