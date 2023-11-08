2f3f042464f47f935b9c27d44171156111aa78b9
Ton Ancheta <ton@xurpas.com>
2023-11-08 12:54:30 +0800

    v1.5.0d - Adjustment to Monthly Behavioral form

--- weekly_planner/__init__.py
+++ weekly_planner/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.5.0c'
+__version__ = '1.5.0d'
 
 def get_version():
     return __version__
--- weekly_planner/weekly_planner/doctype/monthly_behavioral_assessment/monthly_behavioral_assessment.js
+++ weekly_planner/weekly_planner/doctype/monthly_behavioral_assessment/monthly_behavioral_assessment.js
@@ -36,11 +36,11 @@ frappe.ui.form.on("Monthly Behavioral Assessment", {
             frm.set_value("assess_month", month);
         } else {
             // Prevent any changes after saving to preserve document name and the integrity of the learning areas generated
-            ins = frm.get_field('instructor');
+            inst = frm.get_field('instructor');
             st = frm.get_field('student');
             yr = frm.get_field('assess_year');
             mo = frm.get_field('assess_month');
-            ins.$input.prop('readonly', true);
+            inst.$input.prop('readonly', true);
             st.$input.prop('readonly', true);
             yr.$input.prop('readonly', true);
             mo.$input.prop('readonly', true);
