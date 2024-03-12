e7c701951ffae1041345f98bbcef097ba5999b7b
iaiaian1 <yapjakebrian1@gmail.com>
2024-03-12 14:34:07 +0800

    Move the sales report to schoolext

--- weekly_planner/weekly_planner/report/sales_by_student_type/sales_by_student_type.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023, Servio and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["Sales by Student Type"] = {
-	"filters": [
-
-	]
-};
